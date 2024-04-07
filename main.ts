0d5350b668bcb5996c776970c5e0140e76038e27
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-04-07 11:00:12 +0500

    Operators

---
+++ main.ts
@@ -0,0 +1,30 @@
+// Combining Operators
+let sum = 3 + 4 * 5;
+sum++ 
+console.log(sum);
+// Answer will be  60 , 24 , 61 , 53
+
+let mathsum = 5 + 2 * 10;
+mathsum++ 
+console.log(mathsum);
+// Answer will be 16 , 30 , 31 , 26
+
+let ab = 8 - 6 * 5
+ab ++
+console.log(ab);
+// Answer will be 21 , 34 , 0 , -21
+
+let xyz = 24 + 35 * 2 / 2
+xyz ++
+console.log(xyz);
+// Amswer will be 12 , 90 , 58 , 60
+
+let result = 9 / 3 - (9 * 4)
+result ++
+console.log(result)
+// Answer will be 78 , 36 , -32 , 12
+
+let num = 16 / 8 - 5 + 4
+num ++ 
+console.log(num);
+// Answer will be 5 , -5 , 34 , 2
